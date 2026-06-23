601ddc8f6c122f9080a423b4dde8178725055b29
Poul <57594294+torconsult-dev@users.noreply.github.com>
2026-06-23 12:58:46 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rson-eact-v0_1_5-sketch-friendly';
+const CACHE='rson-eact-v0_1_5_1-sketch-undo-date';
 const ASSETS=['./','./index.html','./Паспорт_объекта_app.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
