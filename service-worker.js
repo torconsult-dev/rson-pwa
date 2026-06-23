61240703cd034c47143c28e12cc9c73c58a41628
Poul <57594294+torconsult-dev@users.noreply.github.com>
2026-06-23 20:37:22 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='rson-eact-one-act-v0.2.1';
+const ASSETS=['./','./index.html','./Паспорт_объекта_app.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
